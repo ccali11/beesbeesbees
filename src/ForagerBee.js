8d8a254c710a14e5b6d229df3ff484a1f093ed98
Chris Cali <ChristopherCali1415@gmail.com>
2020-08-24 15:24:49 -0400

    Implement ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  //Bee.call(this);
 };
 
+// Delegate prototype chain to Bee
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+// Own prototype Properties and Methods
+ForagerBee.prototype.age = 10;
+ForagerBee.prototype.job = 'find pollen';
+ForagerBee.prototype.canFly = true;
+ForagerBee.prototype.treasureChest = [];
+ForagerBee.prototype.forage = function () {
+  this.treasureChest.push('treasure');
+}
+
+// Create own constructor property
+ForagerBee.prototype.constructor = ForagerBee;
